85deb22a5ae12f24a6d0ba3ed6b4e464326f5b44
everythinginmoderation2 <everythinginmoderation2@gmail.com>
2022-01-21 21:01:39 +0700

    mobile repsonsiveness start

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -15,18 +15,20 @@ const HomePage = () => {
             <section className='section-body'>
                 <div className="banner"><img src="https://github.com/everythinginmoderation2/prince-slater-client/blob/16f6a5e8361d0def4d5fec9a172c0961df6e4dce/src/Images/hand_and_guitar.jpeg" alt=""></img></div>
                 <div className="div-body">
-                    <div className="blue"></div>
-                    <div className="green"></div>
-                    <article className="article1">
+                    <div className="back-top">
+                        {/* <text className='text-scroll'>Visit</text> */}
+                    </div>
+                    <div className="back-bottom"></div>
+                    <div className="front">
+                        <div className="front-left"></div>
+                        <div className="front-right"></div>
+                    </div>
+                </div>
+                <footer className="footer">
                     <h1>Contact Me</h1>
-                    </article>
-                </div>
+                </footer>
             </section>
-            <section className='section-footer'>
-                <div className='div-footer'>
-                    <Footer className='footer' />
-                </div>
-            </section>
+
         </div>
     )
 }
