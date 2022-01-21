c0178cf57171df6d3142632d5fdd458aada1a3ed
everythinginmoderation2 <everythinginmoderation2@gmail.com>
2022-01-22 01:34:06 +0700

    rel=noreferrer added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -29,12 +29,12 @@ const HomePage = () => {
                     <div className="back-bottom"></div>
                     <div className="front">
                         {/* SoundCloud & YouTube */}
-                        <div className="front-left"><iframe title="PS" width="100%" height="100%" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1116844174&color=%23031b44&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" /><div style={{ fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: 100 }}><a href="https://soundcloud.com/prince-slater" title="PS" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }}>PS</a> · <a href="https://soundcloud.com/prince-slater/wait-any-more-longer-more" title="Wait Any Longer (More Longer)" target="_blank" style={{ color: '#cccccc', textDecoration: 'none' }}>Wait Any Longer (More Longer)</a></div></div>
+                        <div className="front-left"><iframe title="PS" width="100%" height="100%" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1116844174&color=%23031b44&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" /><div style={{ fontSize: '10px', color: '#cccccc', lineBreak: 'anywhere', wordBreak: 'normal', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif', fontWeight: 100 }}><a href="https://soundcloud.com/prince-slater" title="PS" target="_blank" rel="noreferrer" style={{ color: '#cccccc', textDecoration: 'none' }}>PS</a> · <a href="https://soundcloud.com/prince-slater/wait-any-more-longer-more" title="Wait Any Longer (More Longer)" target="_blank" rel="noreferrer" style={{ color: '#cccccc', textDecoration: 'none' }}>Wait Any Longer (More Longer)</a></div></div>
                         <div className="front-right"><iframe title="PS" width="100%" height="100%" src="https://www.youtube.com/embed/oY2h3aBlmQ8" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen /></div>
                     </div>
                 </div>
                 <footer className="footer">
-                <h1><a href="https://www.instagram.com/aneducatedguesser/" target="_blank">Contact Me</a></h1>
+                <h1><a href="https://www.instagram.com/aneducatedguesser/" target="_blank" rel="noreferrer">Contact Me</a></h1>
                 </footer>
             </section>
 
