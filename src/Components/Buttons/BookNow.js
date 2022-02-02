3f54e31a9e0f58df074062830ad5951ca46e26db
everythinginmoderation2 <everythinginmoderation2@gmail.com>
2022-02-03 03:54:13 +0700

    navbar style complete

---
+++ src/Components/Buttons/BookNow.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BookNow = () => {
+    return <div>
+        {/* Calendly link widget */}
+       
+    </div>;
+};
+
+export default BookNow;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Navbar.css';
+import BookNow from '../Buttons/BookNow'
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div>
-            <nav className="navbar">
-                <div className="links">
-                    <div><Link to="/">Home</Link></div>
-                    <div><Link to="/streaming">Streaming</Link></div>
-                    <div><Link to="/upcoming">What's next</Link></div>
-                    <div><Link to="/contact">Contact</Link></div>
-                </div>
-            </nav>
+        <div className="navbar">
+            <div className="logo-container">
+                <Link to="/">
+                    <div className="logo-image" />
+                </Link>
+            </div>
+            <ul className='links'>
+                <li><Link to="/">home</Link></li>
+                <li><Link to="/music">stream now</Link></li>
+                <li><Link to="/about">about</Link></li>
+                <li><Link to="/contact">contact</Link></li>
+            </ul>
         </div>
     )
 }
 
 export default Navbar
 
-{/* <div className="icon"><Link to="/"><h1>Image Icon Here</h1></Link></div> */ }
