738897928c841793427a193ed220ca9e2682ebba
everythinginmoderation2 <everythinginmoderation2@gmail.com>
2022-02-03 06:25:35 +0700

    body started

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,14 +6,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div className="navbar">
-            <div className="logo-container">
-                <Link to="/">
-                    <div className="logo-image" />
-                </Link>
-            </div>
             <ul className='links'>
                 <li><Link to="/">home</Link></li>
-                <li><Link to="/music">stream now</Link></li>
+                <li><Link to="/music">stream</Link></li>
                 <li><Link to="/about">about</Link></li>
                 <li><Link to="/contact">contact</Link></li>
             </ul>
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,17 +1,30 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
 import './HomePage.css'
 import '../Misc/Scrollbar.css'
+import './Max700.css'
+import Navbar from '../../Components/Navbar/Navbar'
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
     return (
-        <div className="root">
-            <section className='header'>
-                    <Navbar className='navbar' />
-            </section>
-            <section className='body'>
-               
-            </section>
+        <div className='root'>
+            <header className='header'>
+                <Navbar />
+            </header>
+            <body className='body'>
+                <row>
+                    <col></col>
+                </row>
+                <row></row>
+                <row></row>
+            </body>
+            <footer>
+            <Link to="/"><div className="logo-image" /></Link>
+            <button className="booking-button">
+            <a href="https://calendly.com/ps--39/singer-songwriter-training-sessions" 
+            target="_blank"  
+            rel="noreferrer">Book <br/> Now</a></button>
+            </footer>
         </div>
     )
 }
