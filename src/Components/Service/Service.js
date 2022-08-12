4d336c7f042f961b919805811e96718206609146
everythinginmoderation2 <everythinginmoderation2@gmail.com>
2022-08-13 01:12:58 +0700

    description fonts for large and medium sized screens

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -12,10 +12,10 @@ function Service() {
                 <caption className="description">My lessons guarantee that you will obtain the knowledge, skill-level and
                 confidence to learn independently and at your own pace.
                 This could likely happen sooner than 8 sessions, but it is recommended to
-                practice with me for as long as desired for the best results.</caption>
+                practice with me for as long as one year for the best results.</caption>
                 <caption className="description-right">If you are training to perform, we will choose a genre that you 
-                wish to play. Then we will choose to learn and practice three songs belonging to that genre. Otherwise, if
-                you desire to play only your favorites or for fun, we will practice the specific songs.</caption>
+                wish to play. Then, we will choose to learn and practice three songs belonging to that genre. Otherwise, if
+                you desire to play only your favorites or for fun, we will practice these specific songs.</caption>
             </div>
             <button className="booking-list-button">
                     <a href="https://calendly.com/ps--39/learn-a-song"
