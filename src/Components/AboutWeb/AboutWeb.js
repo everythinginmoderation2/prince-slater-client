44754440a76538603d7d088f9345a9cded8d0bb2
Prince Slater <everythingindmoderation2@gmail.com>
2025-09-25 15:24:49 -0400

    .

--- src/Components/AboutWeb/AboutWeb.js
+++ src/Components/AboutWeb/AboutWeb.js
@@ -5,10 +5,6 @@ const AboutWeb = () => {
     return (
         <div className='caption-overlay'>
             <p className='about-web-p'>I built this webpage using JavaScript, React.js, and CSS.</p>
-            <button className='more-info'><a href="https://forms.gle/JAB9RbM8QSLVBTFg7"
-                target="_blank"
-                rel="noreferrer">More Info</a></button>
-            <p className='about-web-p'>I can do the same for you.</p>
         </div>
     )
 }
