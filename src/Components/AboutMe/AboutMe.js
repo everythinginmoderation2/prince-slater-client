ac51852d2f495180478baa42756203c3b04f9dce
Prince Slater <everythingindmoderation2@gmail.com>
2025-09-25 15:05:40 -0400

    edits

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -8,15 +8,13 @@ function AboutMe() {
                 <h1 className="row-title-about">About Me</h1>
                 <div className="p-container">
                 <p className="about">A writer, singer, instrumentalist and performer who enjoys writing and arranging 
-                music for himself, as well as, for others. Or more simply put, a performing arts enthusiast.</p>
+                music for himself and others.</p>
 
                 <p className="about">Since my  early practice sessions as a solo performer
                 began in 2016, much of my humble beginnings took off in some of Vietnam's hot-spots, popular cafes, restaurants,
                 studios and venues. I started off at open mic nights, then continued at exclusive 
-                gatherings and even organized events like talent showcases and festivals. Send a msg for inquiries.</p>
-                <address className='about-contact'>
-                <a href="mailto:ps@princeslater.com">ps@princeslater.com</a>
-                </address>
+                    gatherings and even organized events like talent showcases and festivals.</p>
+                
                 <p className="about">My experience in the entertainment industry extends further
                 than any guitar or mic cable. A lot of extra-curricular dedication has allowed me to take part in  
                 productions on stage and in front of (or behind) cameras. Whether it be as a model, thespian, musician, emcee,
